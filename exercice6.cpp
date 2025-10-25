75551b9636f387f8aa4380d135e60598d8bc703b
Oboualiti <122377581+Oboualiti@users.noreply.github.com>
2025-10-25 22:14:05 +0100

    Add files via upload

--- exercice6.cpp
+++ exercice6.cpp
@@ -1,1 +1,29 @@
-jr
+#include <iostream>
+using namespace std;
+
+
+void incrementer(int *p) {
+    (*p)++; 
+}
+
+
+void permuter(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+
+    cout << "Avant incrementer: x = " << x << endl;
+    incrementer(&x);
+    cout << "Apres incrementer: x = " << x << endl;
+
+    cout << "\nAvant permuter: x = " << x << ", y = " << y << endl;
+    permuter(&x, &y);
+    cout << "Apres permuter: x = " << x << ", y = " << y << endl;
+
+    return 0;
+}
+
